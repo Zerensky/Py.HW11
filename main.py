8fcacc93457b59536e538ae38c3faced51aecfc6
Zerensky <pure.evil97@mail.ru>
2023-07-09 22:20:12 +0600

    main file

---
+++ main.py
@@ -0,0 +1,10 @@
+from Python_next_deep.Seminar_11.my_str import run_mystring
+from Python_next_deep.Seminar_11.archive import run_archive
+from Python_next_deep.Seminar_11.rectangle import run_rectangle
+from Python_next_deep.Seminar_11.matrix import run_matrix
+
+if __name__ == '__main__':
+    run_mystring()
+    run_archive()
+    run_rectangle()
+    run_matrix()
